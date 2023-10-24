8361126943133c9fed7343e303ddbb945aee890c
natiapreutesei <natiapreutesei@gmail.com>
2023-10-24 14:15:52 +0200

    changes

--- oefeneningen/oefening3/assets/js/index.js
+++ oefeneningen/oefening3/assets/js/index.js
@@ -1,8 +1,8 @@
-function pow(base,exponent){ //function that asks for two parameters, the base and the exponent
-    let result = base; //initializing the result with the same number that we gave for base // ex. base=2; result=2;
-    for (let i=1;i<exponent; i++){   //it repeats the circle as many times as the exponent
-        result = result*base;  //we store the value of r*b in result. that means that the first iteration we will have 2=2*2
-    }                          //then the second iterations 4=4*2 with the result being 8
+function pow(base,exponent){          //function that asks for two parameters, the base and the exponent
+    let result = base;               //initializing the result with the same number that we gave for base // ex. base=2; result=2;
+    for (let i=1;i<exponent; i++){  //it repeats the circle as many times as the exponent
+        result = result*base;      //we store the value of r*b in result. that means that the first iteration we will have 2=2*2
+    }                             //then the second iterations 4=4*2 with the result being 8
     return result;
 }
 
